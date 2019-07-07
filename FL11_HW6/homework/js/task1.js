1b547145d2b294e4d12e90a4d3fd681b1a0a5dfc
Andreas_Just <andreas.wq@gmail.com>
2019-07-07 06:06:30 +0300

    Task 1 was implemented

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,23 @@
-// Your code goes here
+const coordNames = ['a1', 'a2', 'b1', 'b2', 'c1', 'c2'];
+const coord = {};
+const err = 0.0000000001;
+
+
+for (let index = 0; index < coordNames.length; index++) {
+  const current = prompt(`Input the coordinate ${coordNames[index]}: `, '');
+
+  if (isNaN(+current) || !isFinite(+current) || current === null || current === '') {
+    alert(`«${current}» - it is not number, please press F5 and enter number`);
+    break;
+  } else {
+    coord[coordNames[index]] = +current;
+  }
+}
+
+console.log(
+  Math.abs(coord['a1'] + coord['b1'] - coord['c1'] - coord['c1']) < err &&
+  Math.abs(coord['a2'] + coord['b2'] - coord['c2'] - coord['c2']) < err
+);
+
+
+
