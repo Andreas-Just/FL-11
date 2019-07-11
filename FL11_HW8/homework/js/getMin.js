69e856fa0d1a4fcf697ae7a110d7af18c7208e7d
Andreas_Just <andreas.wq@gmail.com>
2019-07-11 09:12:29 +0300

    Task getMin was implemented #2

--- FL11_HW8/homework/js/getMin.js
+++ FL11_HW8/homework/js/getMin.js
@@ -1,13 +1,5 @@
 function getMin() {
-  const sorted = [...arguments]
-    .map(
-      function() {
-        return this.splice(this.indexOf(Math.min(...this)), 1)[0];
-      },
-      [...arguments].slice()
-    );
-
-  return sorted[0];
+  return Math.min.apply(null, [...arguments]);
 }
 
 console.log(getMin(5, 2, 12, -2, 40, -15, 0));
