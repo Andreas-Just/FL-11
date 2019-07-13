db8efc4d2730f01bb71b9393964b8296c9a27e7b
Andreas_Just <andreas.wq@gmail.com>
2019-07-13 12:02:29 +0300

    Task pipe was implemented #2

--- FL11_HW8/homework/js/pipe.js
+++ FL11_HW8/homework/js/pipe.js
@@ -3,9 +3,7 @@ const addOne = x => x + 1;
 function pipe(num) {
   const [, ...callbacks] = arguments;
 
-  return [...callbacks].reduce((acc, curr) => {
-    acc = curr(acc); return acc;
-  }, num);
+  return [...callbacks].reduce((acc, curr) => curr(acc), num);
 }
 
 console.log(pipe(1, addOne, addOne));
