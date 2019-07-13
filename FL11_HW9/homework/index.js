3eb2821da055cad4486fcef71ef40289b671f517
Andreas_Just <andreas.wq@gmail.com>
2019-07-13 20:34:49 +0300

    task 3 & 4 were implemented

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -17,10 +17,34 @@ console.log(getNumbers('n1um3ber95')); // returns [1,3,9,5]
 
 
 // task 2
-function executeforEach(arr, callback) {
+const executeforEach = (arr, callback) => {
   for (let item of arr) {
     callback(item);
   }
-}
+};
 
-executeforEach([1,2,3], function(el) { console.log(el) });
+executeforEach([1, 2, 3], function(el) {
+  console.log(el)
+}); // logs 1 2 3
+
+// task 3
+const mapArray = (arr, callback) => {
+  const result = [];
+  executeforEach(arr,el => result.push(callback(el)));
+  return result;
+};
+
+console.log(
+  mapArray([2, 5, 8], el => el + 3) // returns [5, 8, 11]
+);
+
+// task 4
+const filterArray = (arr, callback) => {
+  let result = [];
+  executeforEach(arr,el => callback(el) ? result.push(el) : null);
+  return result;
+};
+
+console.log(
+  filterArray([2, 5, 8], el => el > 3) // returns [5, 8]
+);
