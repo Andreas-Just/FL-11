2f37ff906be4585c59d5452dd2ffd763c98bce15
Andreas_Just <andreas.wq@gmail.com>
2019-07-13 18:23:52 +0300

    task 0 was implemented

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// task 0
+
+const getNumbers = (str) => {
+  const regExp = /\d/g;
+  const result = [];
+  if (regExp.test(str)) {
+    for (let item of [...str.match(regExp)]) {
+      result.push(+item);
+    }
+  }
+  return result;
+};
+
+console.log(getNumbers('string')); // returns []
+console.log(getNumbers('n1um3ber95')); // returns [1,3,9,5]
