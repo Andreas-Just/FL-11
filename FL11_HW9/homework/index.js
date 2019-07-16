8bdbc42bdc4a25518fb91c92a94939f3d56ed6cf
Andreas_Just <andreas.wq@gmail.com>
2019-07-16 11:01:51 +0300

    task 1,5,6,7 were implemented

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -14,6 +14,22 @@ console.log(getNumbers('string')); // returns []
 console.log(getNumbers('n1um3ber95')); // returns [1,3,9,5]
 
 // task 1
+const findTypes = (...args) => {
+  const result = {};
+  for (let item of args) {
+    item = typeof item;
+    if (!result.hasOwnProperty(item)) {
+      result[item] = 1;
+    } else {
+      result[item]++;
+    }
+  }
+  return result;
+};
+
+console.log(findTypes('number', 5, 5, {}));
+console.log(findTypes(null, 5, 'hello', [], getNumbers));
+
 
 
 // task 2
@@ -48,3 +64,33 @@ const filterArray = (arr, callback) => {
 console.log(
   filterArray([2, 5, 8], el => el > 3) // returns [5, 8]
 );
+
+// task 5
+const showFormattedDate = (date) => {
+  const variant = {day: '2-digit', month: 'short'};
+  const dateFormat = date.toLocaleDateString('en-US', variant);
+  return `'Date: ${dateFormat} ${date.getFullYear()}'`;
+};
+
+console.log(showFormattedDate(new Date('2019-01-27T01:10:00')));
+console.log(showFormattedDate(new Date('2019-02-29T01:10:00')));
+
+// task 6
+const canConvertToDate = (str) => {
+  return !isNaN(Date.parse(str));
+};
+
+console.log(canConvertToDate('2016-13-18T00:00:00')); // false
+console.log(canConvertToDate('2016-03-18T00:00:00')); // true
+
+// task 7
+const daysBetween = (dateBefore, dateAfter) => {
+  const diff = Math.abs(dateAfter - dateBefore);
+  const day = 1000 * 60 * 60 * 24;
+  return Math.ceil( diff / day);
+};
+
+console.log(
+  daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00'))
+); // 32
+
