7d6880a01e2ae92672eec1ed4fa402ef7a2a1af9
Andreas_Just <andreas.wq@gmail.com>
2019-07-18 02:42:36 +0300

    training

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,1 +1,41 @@
-/* Your code goes here */
+const father = {
+  firstName: 'Nick',
+  lastName: 'Hrynko',
+
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  },
+};
+
+/*
+User.prototype = father;
+function User(name) {
+  // this = {};
+  // this.__proto__ = father;
+
+  this.firstName = name;
+  // return this;
+}
+
+class User {
+  constructor(name) {
+    // this = {};
+    this.firstName = name;
+    this.__proto__ = father;
+    // return this;
+  }
+}
+*/
+
+class User2 {
+  constructor(name) {
+    //  this.__proto__ = father;
+    this.firstName = name;
+    // Object.setPrototypeOf(this, father);
+  }
+  sayHi() {}
+}
+Object.setPrototypeOf(User2.prototype, father);
+
+const me = new User2('Misha');
+console.log(me);
