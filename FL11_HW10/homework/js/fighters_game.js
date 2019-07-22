6b974614b85db63dc4097ea7da6c6e02ff5180ff
Andreas_Just <andreas.wq@gmail.com>
2019-07-22 19:55:57 +0300

    The battle between the fighters was held

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,38 +1,100 @@
-const User = (() => {
-  let role = 'user1';
+class Fighter {
+  constructor(element) {
+    this._el = element;
+    this._initiallyHP = this._el.hp;
+    this._wins = 0;
+    this._losses = 0;
+  }
+  getName() {
+    return this._el.name;
+  }
+  getDamage() {
+    return this._el.damage;
+  }
+  getHealth() {
+    return this._el.hp;
+  }
+  getAgility() {
+    return this._el.agility;
+  }
+  attack(fighter) {
+    const maxLikeliness = 100;
+    const likeliness = maxLikeliness - fighter.getAgility();
+    const chanceAttack = Math.round(Math.random() * maxLikeliness);
 
-  return class {
-    name = 'Andre';
-
-    test() {
-      console.log(role);
+    if (chanceAttack <= likeliness) {
+      fighter.dealDamage(this.getDamage());
+      console.log(
+        `${this.getName()} make ${this.getDamage()} damage to ${fighter.getName()}`
+      );
+    } else {
+      console.log(`${this.getName()} attack missed`);
     }
-  };
-})();
-
-const andre = new User();
-andre.test();
-console.log(andre.name);
-console.log(andre.role);
-
-// variant 2
-const factoryFromMisha = () => {
-  let role = 'user2';
-
-  return class {
-    name = 'Misha';
-
-    test() {
-      console.log(role);
+  }
+  logCombatHistory() {
+    console.log(
+      `Name: ${this._el.name}, Wins: ${this._wins}, Losses: ${this._losses}`
+    );
+  }
+  heal(hp) {
+    if (this._el.hp + hp > this._initiallyHP) {
+      this._el.hp = this._initiallyHP;
+    } else {
+      this._el.hp += hp;
     }
-  };
+    return this._el.hp;
+  }
+  dealDamage(hp) {
+    if (this._el.hp - hp < 0) {
+      this._el.hp = 0;
+    } else {
+      this._el.hp -= hp;
+    }
+    return this._el.hp;
+  }
+  addWin() {
+    return this._wins++;
+  }
+  addLoss() {
+    return this._losses++;
+  }
+
+}
+
+const battle = (fighter1, fighter2) => {
+  if (!fighter1.getHealth() || !fighter2.getHealth()) {
+    const nameOfDead = !fighter1.getHealth()
+      ? fighter1.getName()
+      : fighter2.getName();
+    return console.log(`${nameOfDead} is dead and can't fight`);
+  }
+
+  while (fighter1.getHealth() > 0 && fighter2.getHealth() > 0) {
+    fighter1.getHealth() > 0 ? fighter1.attack(fighter2) : null;
+    fighter2.getHealth() > 0 ? fighter2.attack(fighter1) : null;
+  }
+
+  fighter1.getHealth() > 0 ? fighter1.addWin() : fighter1.addLoss();
+  fighter2.getHealth() > 0 ? fighter2.addWin() : fighter2.addLoss();
 };
 
-const User2 = factoryFromMisha();
-const misha = new User2();
+const john = new Fighter(
+  {name: 'John', damage: 20, agility: 25, hp: 100}
+);
+const jim = new Fighter(
+  {name: 'Jim', damage: 10, agility: 40, hp: 120}
+);
 
-misha.test();
-console.log(misha.name);
-console.log(misha.role);
+battle(john, jim);
+john.logCombatHistory();
+jim.logCombatHistory();
 
+john.heal(70);
+jim.heal(140);
+console.log(john.getHealth());
+console.log(jim.getHealth());
+
+battle(john, jim);
+john.logCombatHistory();
+jim.logCombatHistory();
 
